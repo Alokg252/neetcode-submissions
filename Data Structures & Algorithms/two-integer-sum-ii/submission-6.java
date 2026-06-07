5d1f03d84024387b9829b77ef9775e6236fc4691
Alok Gupta <73645127+Alokg252@users.noreply.github.com>
2026-06-07 16:45:13 +0530

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int l = nums.length, s = 0, e = l-1;
+        while (s < e) {
+            int sum = nums[s] + nums[e];
+            if (sum < target) s++;
+            else if (sum > target) e--;
+            else break;
+        }
+        return new int[]{s+1, e+1};
+    }
+}

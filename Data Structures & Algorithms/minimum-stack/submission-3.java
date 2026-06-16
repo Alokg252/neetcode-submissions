5952b55e716fa93ce0486353f7c36260d4fcd5b8
Alok Gupta <73645127+Alokg252@users.noreply.github.com>
2026-06-16 12:59:31 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,49 @@
+
+class MinStack {
+    Node head;
+
+    public MinStack() {
+        Node head = null;
+    }
+
+    public void push(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            node.min = val;
+            node.next = null;
+        }
+        else {
+            node.min = Math.min(head.min, val);
+            node.next = head;
+        }
+        head = node;
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+
+    static class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node() {}
+        public Node(int val) {this.val = val;}
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+}
